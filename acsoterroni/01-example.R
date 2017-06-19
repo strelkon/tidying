8c844430f20335348f2d62cbcb26216ad71a590f
Nike <strelkon@iiasa.ac.at>
2017-06-19 13:26:16 +0200

    Added CSV red/write

--- acsoterroni/01-example.R
+++ acsoterroni/01-example.R
@@ -8,4 +8,9 @@ y <- sin(x)
 z <- cos(x)
 plot(x,y,type="l",col="blue",ylab="f(x)")
 lines(x,z,type="l",col="green")
+<<<<<<< HEAD
 title(main="sine and cosine waves")
+=======
+title(main="sine and cosine waves")
+
+>>>>>>> 8f8ac3fc1b7a3dc0b52d6fdef988647a1dcfc584
